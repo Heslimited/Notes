7368b171642a9c5af92a3ccf2c43aaf7ec238636
Heslimited <dimzo333@gmail.com>
2023-07-25 14:25:26 +0300

    Added footer, finished main page

---
+++ src/components/regPageComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="register-container">
+        <div>   
+            <img src="../assets/note.jpg" alt="Note">
+        </div>
+    </div>
+</template>
+
+<style>
+
+    
+</style>
